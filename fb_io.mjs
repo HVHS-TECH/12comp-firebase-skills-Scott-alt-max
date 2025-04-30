6553f59d4a9787e5a7279c6ca076cec462d5492c
Scott-alt-max <22069sb@hvhs.school.nz>
2025-04-30 21:58:51 +0000

    did something mabye

--- fb_io.mjs
+++ fb_io.mjs
@@ -25,8 +25,30 @@ export {
     fb_initialise };
 
 function fb_initialise() {
-    console.log('%c fb_initialise(): ', 
-                'color: ' + COL_C + '; background-color: ' + COL_B + ';');
+    console.log('%c fb_initialise(): ', 'color: ' + COL_C + '; background-color: ' + COL_B + ';');
+    
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+    import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+    
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+        apiKey: "AIzaSyBA9LF4VKTGLBynVTOiG3iJqm-odKKE74g",
+        authDomain: "comp-2025-scott-barlow.firebaseapp.com",
+        databaseURL: "https://comp-2025-scott-barlow-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "comp-2025-scott-barlow",
+        storageBucket: "comp-2025-scott-barlow.firebasestorage.app",
+        messagingSenderId: "604831891804",
+        appId: "1:604831891804:web:e1d0c36b49a9ad732b4199",
+        measurementId: "G-5JBDKMXH4C"
+    };
+    
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
 }
 
 /**************************************************************/
