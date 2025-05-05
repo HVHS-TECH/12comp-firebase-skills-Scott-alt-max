87858de1467e5f8942f0adc33a92b32d74e53307
Scott-alt-max <22069sb@hvhs.school.nz>
2025-05-05 00:27:31 +0000

    finsihed task 4

--- fb_io.mjs
+++ fb_io.mjs
@@ -1,41 +1,40 @@
 //**************************************************************/
 // fb_io.mjs
 // Generalised firebase routines
-// Written by <Your Name Here>, Term 2 202?
+// Written by Scott, Term 2 202?
 //
 // All variables & function begin with fb_  all const with FB_
 // Diagnostic code lines have a comment appended to them //DIAG
 /**************************************************************/
 const COL_C = 'white';	    // These two const are part of the coloured 	
 const COL_B = '#CD7F32';	//  console.log for functions scheme
-console.log('%c fb_io.mjs',
-            'color: blue; background-color: white;');
+console.log('%c fb_io.mjs', 'color: blue; background-color: white;');
 
 /**************************************************************/
 // Import all external constants & functions required
 /**************************************************************/
 // Import all the methods you want to call from the firebase modules
 
-
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
 /**************************************************************/
 // EXPORT FUNCTIONS
 // List all the functions called by code or html outside of this module
 /**************************************************************/
-export { 
-    fb_initialise };
+export { fb_initialise, fb_authenticate };
 
 function fb_initialise() {
     console.log('%c fb_initialise(): ', 'color: ' + COL_C + '; background-color: ' + COL_B + ';');
-    
-    // Import the functions you need from the SDKs you need
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
-    import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
+
     // TODO: Add SDKs for Firebase products that you want to use
     // https://firebase.google.com/docs/web/setup#available-libraries
     
     // Your web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    const firebaseConfig = {
+    const FB_GAMECONFIG = {
         apiKey: "AIzaSyBA9LF4VKTGLBynVTOiG3iJqm-odKKE74g",
         authDomain: "comp-2025-scott-barlow.firebaseapp.com",
         databaseURL: "https://comp-2025-scott-barlow-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -46,11 +45,26 @@ function fb_initialise() {
         measurementId: "G-5JBDKMXH4C"
     };
     
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
+    const FB_GAMEAPP = initializeApp(FB_GAMECONFIG);
+    const FB_GAMEDB = getDatabase(FB_GAMEAPP);
+    console.info(FB_GAMEDB); //DIAG
 }
+function fb_authenticate() {
+    console.log('%c fb_authenticate: ', 'color: ' + COL_C + '; background-color: ' + COL_B + ';');
 
-/**************************************************************/
-// END OF CODE
-/**************************************************************/
+    const AUTH = getAuth();
+    const PROVIDER = new GoogleAuthProvider();
+
+    // The following makes Google ask the user to select the account
+    PROVIDER.setCustomParameters({
+        prompt: 'select_account'
+    });
+    signInWithPopup(AUTH, PROVIDER).then((result) => {
+        console.log("Authentication successful");
+        console.log(result); //DIAG
+    })
+    .catch((error) => {
+        console.log("Authentication unsuccessful");
+        console.log(error); //DIAG
+    });
+}
--- main.mjs
+++ main.mjs
@@ -12,9 +12,9 @@ console.log('%c main.mjs',
 // Import all external constants & functions required
 /**************************************************************/
 // Import all the constants & functions required from fb_io module
-import { fb_initialise }
-    from './fb_io.mjs';
-    window.fb_initialise   = fb_initialise;
+import { fb_initialise, fb_authenticate } from './fb_io.mjs';
+window.fb_initialise = fb_initialise;
+window.fb_authenticate = fb_authenticate;
  
 /**************************************************************/
 // index.html main code
